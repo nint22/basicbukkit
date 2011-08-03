a403e5e38f36f3b1715b2fccc6a26ed76ce1f2db
nint22@gmail.com <nint22@gmail.com@0c34c27e-d5e0-5370-6de2-49b41a7cadb1>
2011-08-03 21:32:21 +0000

    Moving file names

---
+++ src/nint22/basicbukkit/BasicEconomy.java
@@ -0,0 +1,20 @@
+/***************************************************************
+ 
+ BasicBukkit - Basic commands for any new MineCraft server
+ Copyright 2011 Core S2 - See License.txt for info
+ 
+ This source file is developed and maintained by:
+ + Jeremy Bridon jbridon@cores2.com
+ 
+ File: BasicDaemon.java
+ Desc: Creates a single thread that prints out messages defined
+ in the server configuration after every x number of seconds.
+ 
+***************************************************************/
+
+package nint22.basicbukkit;
+
+public class BasicEconomy
+{
+    
+}
---
+++ src/nint22/basicbukkit/BasicEconomyCommands.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package nint22.basicbukkit;
+
+/**
+ *
+ * @author nint22
+ */
+public class BasicEconomyCommands {
+    
+}
